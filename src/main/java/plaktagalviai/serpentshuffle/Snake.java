cc74128934753dfa2329c93893f2925ab1175a0d
benasraudys <benas.raudys@gmail.com>
2024-05-30 10:13:35 +0300

    Refactor classes

--- src/main/java/plaktagalviai/serpentshuffle/Snake.java
+++ src/main/java/plaktagalviai/serpentshuffle/Snake.java
@@ -1,15 +1,12 @@
 package plaktagalviai.serpentshuffle;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.layout.Pane;
 
 public class Snake extends ArrayList<SnakeSegment> {
-
-    /*private ArrayList<SnakeSegment> snake;
-
-    public Snake(ArrayList<SnakeSegment> snake) {
-        this.snake = snake;
-    }*/
 
     public SnakeSegment getHead() {
         return this.isEmpty() ? null : this.get(0);
@@ -19,8 +16,57 @@ public class Snake extends ArrayList<SnakeSegment> {
         return this.isEmpty() ? null : this.get(this.size() - 1);
     }
 
-    /*private boolean snakeCollidedWithWall(int GRID_SUBDIVISIONS) {
-        return (snake.getFirst().getX() > GRID_SUBDIVISIONS - 1) || (snake.getFirst().getY() > GRID_SUBDIVISIONS - 1) ||
-                (snake.getFirst().getX() < 0) || (snake.getFirst().getY() < 0);
-    }*/
+    public void addSegment(Pane root, int subdivisionLength) {
+        SnakeSegment last = this.getTail();
+        SnakeSegment newSegment = new SnakeSegment(last.getRectangle().getX(),
+                last.getRectangle().getY(), subdivisionLength, subdivisionLength);
+        Image image = new Image(getClass().getResourceAsStream("snake-body.png"));
+        newSegment.getRectangle().setFill(new ImagePattern(image));
+        this.add(newSegment);
+        root.getChildren().add(newSegment.getRectangle());
+    }
+
+    public void move() {
+        double prevX, prevY, newX, newY, prevDx, prevDy, newDx, newDy;
+        prevX = this.getHead().getX();
+        prevY = this.getHead().getY();
+        prevDx = this.getHead().getDx();
+        prevDy = this.getHead().getDy();
+
+        this.getHead().move();
+
+        for (int i = 1; i < this.size(); i++) {
+            SnakeSegment segment = this.get(i);
+            newX = segment.getX();
+            newY = segment.getY();
+            newDx = segment.getDx();
+            newDy = segment.getDy();
+            segment.setX(prevX);
+            segment.setY(prevY);
+            segment.setDirection(prevDx, prevDy);
+            segment.updateRectangle();
+            prevX = newX;
+            prevY = newY;
+            prevDx = newDx;
+            prevDy = newDy;
+        }
+    }
+
+    public boolean collidedWithWall(int gridSubdivisions) {
+        return this.getHead().getX() >= gridSubdivisions || this.getHead().getY() >= gridSubdivisions ||
+                this.getHead().getX() < 0 || this.getHead().getY() < 0;
+    }
+
+    public boolean collidedWithSelf() {
+        SnakeSegment head = this.getHead();
+        if (head.getDx() == 0 && head.getDy() == 0) {
+            return false;
+        }
+        for (int i = 1; i < this.size(); i++) {
+            if (head.getX() == this.get(i).getX() && head.getY() == this.get(i).getY()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
